a51482e60684947ef6eb54f227c5a3e675e9743b
temu-bire <temesgen3401@gmail.com>
2026-08-28 05:21:53 -0700

    fix(token): set proper jwt error

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,14 +1,17 @@
-import type {Request, Response, NextFunction} from 'express';
-import  {ApiError} from '../utils/apiError.js';
+import type { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/apiError.js";
 import { ZodError } from "zod";
+import jwt from "jsonwebtoken";
+
 export const errorMiddleware = (
   error: unknown,
   _req: Request,
-    res: Response,
-    _next: NextFunction,
+  res: Response,
+  _next: NextFunction,
 ): void => {
   console.error(error);
-   if (error instanceof ZodError) {
+
+  if (error instanceof ZodError) {
     res.status(400).json({
       success: false,
       message: "Validation failed",
@@ -17,16 +20,35 @@ export const errorMiddleware = (
         message: issue.message,
       })),
     });
+    return;
   }
+
   if (error instanceof ApiError) {
     res.status(error.statusCode).json({
       success: false,
-        message: error.message,
+      message: error.message,
     });
     return;
   }
+
+  if (error instanceof jwt.TokenExpiredError) {
+    res.status(401).json({
+      success: false,
+      message: "Access token expired",
+    });
+    return;
+  }
+
+  if (error instanceof jwt.JsonWebTokenError) {
+    res.status(401).json({
+      success: false,
+      message: "Invalid access token",
+    });
+    return;
+  }
+
   res.status(500).json({
     success: false,
-    message:'Internal server error',
+    message: "Internal server error",
   });
 };
